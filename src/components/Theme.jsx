46a02b329cfcb0efc7da9bd863ba7fbd0f8c23b3
Ayush singh <anshupaul28@gmail.com>
2025-10-26 11:29:06 +0530

    spaling

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -4,9 +4,7 @@ export const Theme = () => {
   return (
   <section  id="main" className="py-24 px-4 relative">
     <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
-         My<span className="text-primary"> Projects</span>
-        </h2>
+
     </div>
 
   </section>
